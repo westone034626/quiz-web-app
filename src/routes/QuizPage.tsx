26077b45ce767005fab8aeee7d4a5d9e13209fbe
westone034626 <dydrlaks@likelion.org>
2023-12-13 00:07:37 +0900

    feat: Load quiz data on quiz page

--- src/routes/QuizPage.tsx
+++ src/routes/QuizPage.tsx
@@ -1,6 +1,44 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+interface Quiz {
+    type: "multiple";
+    difficulty: "easy" | "medium" | "hard";
+    category: string;
+    question: string;
+    correct_answer: string;
+    incorrect_answers: [string, string, string];
+}
+
+const API_URL = 'https://opentdb.com/api.php?amount=10';
+
 const QuizPage = () => {
+    const [quizs, setQuizs] = useState<Quiz[]>([]);
+
+    useEffect(() => {
+        const loadQuiz = async () => {
+            try {
+                const data = await axios.get<{ results: Quiz[]; }>(API_URL);
+
+                setQuizs(data.data.results);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        loadQuiz();
+    }, []);
+
     return (
-        <div>QuizPage</div>
+        <>
+            <div>QuizPage</div>
+
+            <ol>
+                {quizs.map(quiz => (
+                    <li>{quiz.question}</li>
+                ))}
+            </ol>
+        </>
     );
 };
 
