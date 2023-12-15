8b509be32f16c0dfbf9591564de8422ab457e4a8
westone034626 <dydrlaks@likelion.org>
2023-12-15 20:58:09 +0900

    feat: Add two buttons UI on quiz page

--- src/routes/QuizPage.tsx
+++ src/routes/QuizPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Quiz from '../components/Quiz';
+import Button from '../components/Button';
 import useQuizs from '../hooks/useQuizs';
 
 const QuizPage = () => {
@@ -22,6 +23,16 @@ const QuizPage = () => {
                 selectedOptionIndex={selectedOption}
                 onSelectOption={setSelectedOption}
             />
+
+            <div className='flex gap-1 justify-center'>
+                <Button onClick={() => console.log('정답 확인')}>
+                    정답 확인
+                </Button>
+
+                <Button onClick={() => console.log('정답 확인')}>
+                    다음 문제
+                </Button>
+            </div>
         </>
     );
 };
