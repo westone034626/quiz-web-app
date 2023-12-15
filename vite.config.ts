4282593307453822490b78dff3be17cf5dd00b4b
westone034626 <dydrlaks@likelion.org>
2023-12-16 00:19:56 +0900

    chore: Set basename correctly

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://westone034626.github.io/quiz-web-app'
+  base: '/quiz-web-app/'
 });
